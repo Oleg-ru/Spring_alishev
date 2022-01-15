eaa766d91c47411dc2a387fb8dafc515b71bdc55
Oleg <oleg_09-95@mail.ru>
2022-01-15 15:27:02 +0300

    lesson-3 applicationContext

---
+++ src/main/java/ru/oleg/Main.java
@@ -0,0 +1,17 @@
+package ru.oleg;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.oleg.beans.TestBean;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
